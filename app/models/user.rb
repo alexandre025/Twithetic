51055559c6e30480d6bf9a3a524f1605a9032a74
alexandre025 <alexandre.ferraille@gmail.com>
2015-11-27 19:02:45 +0100

    User : username + friendly_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  has_one :photo, class_name: 'Asset::User'
+  accepts_nested_attributes_for :photo
+
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127143808) do
+ActiveRecord::Schema.define(version: 20151127175345) do
 
   create_table "assets", force: :cascade do |t|
     t.integer  "viewable_id",             limit: 4
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20151127143808) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
