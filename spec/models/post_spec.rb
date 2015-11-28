56a7e4f33e7ecf8199c1bdf6d6a3d27fa51dbc10
Paul Boiseau <paulboiseau@gmail.com>
2015-11-28 19:34:19 +0100

    add user create test

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Post, :type => :model do
+
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+
+  it 'can be saved' do
+    email = Faker::Internet.email
+    user_name = Faker::Internet.user_name
+
+    user = User.create(email: email, password: Faker::Internet.password, username: user_name)
+    user.save!
+
+    found = User.last
+    expect(found.email).to eq(email)
+    expect(found.username).to eq(user_name)
+  end
+
+  it 'is impossible to add the same username twice' do
+    user_name = Faker::Internet.user_name
+
+    user = User.create(email: Faker::Internet.email, password: Faker::Internet.password, username: user_name)
+    expect(user.valid?).to eq(true)
+
+    other_user = User.create(email: Faker::Internet.email, password: Faker::Internet.password, username: user_name)
+    expect(other_user.valid?).to eq(false)
+  end
+
+end
