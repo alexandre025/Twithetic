a1b6e4f9ed435713e576683723458c2a5249eb18
alexandre025 <alexandre.ferraille@gmail.com>
2015-11-30 15:41:27 +0100

    Create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-
+    @object = Comment.new(permitted_attributes)
+    @object.post_id = params[:post_id]
+    if @object.save
+      flash.now[:notice] = 'add_comment_success'
+    else
+      flash.now[:error] = 'add_comment_error'
+    end
+    redirect_to post_path(@object.post)
   end
 
   def update
@@ -11,5 +18,14 @@ class CommentsController < ApplicationController
   def delete
 
   end
+
+  private
+    def load_resource
+      @object = Comment.find(params[:id])
+    end
+
+    def permitted_attributes
+      params.require(:comment).permit(:message,:user_id)
+    end
 
 end
