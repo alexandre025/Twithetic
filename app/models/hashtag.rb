55d8b3f56370f3a3dfc51012df2741d26fc776a3
Paul Boiseau <paulboiseau@gmail.com>
2015-12-02 16:09:50 +0100

    parse hashtags

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,2 @@
 class Hashtag < ActiveRecord::Base
-
-  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,12 @@
 class Post < ActiveRecord::Base
 
   belongs_to :user
-
   belongs_to :parent, class_name: 'Post'
 
   acts_as_followable
 
   has_one :image, as: :viewable, class_name: 'Asset::PostImage', dependent: :destroy
   accepts_nested_attributes_for :image
-
   has_many :comments, dependent: :destroy
 
   def retweet(by_user)
@@ -29,6 +27,18 @@ class Post < ActiveRecord::Base
 
   def is_a_retweet?
     self.parent.present?
+  end
+
+  def parse_hashtags
+    hashtags = self.message.scan(/(#\w+)/).flatten
+    hashtags.each do |hashtag|
+      tag = hashtag.tr('#', '')
+      if Hashtag.where(name: tag).first.present? 
+		
+      else
+
+      end
+    end
   end
 
 end
