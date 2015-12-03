34ddb9543e4a380c5e57707675c8506afb27b3e8
alexandre025 <alexandre.ferraille@gmail.com>
2015-12-03 15:48:26 +0100

    Header

--- app/models/asset/user_image.rb
+++ app/models/asset/user_image.rb
@@ -1,11 +1,11 @@
 class Asset::UserImage < Asset
 
   has_attached_file :attachment,
-                    styles: { thumb: '48x48>' },
+                    styles: { thumb: '35x35#' },
                     default_style: :thumb,
-                    default_url: '/default/users/:style/noimage.png',
-                    url: '/twithetic/users/:id/:style/:basename.:extension',
-                    path: ':rails_root/public/twithetic/users/:id/:style/:basename.:extension',
+                    default_url: '/default/users/image/:style/noimage.png',
+                    url: '/twithetic/users/:id/image/:style/:basename.:extension',
+                    path: ':rails_root/public/twithetic/users/:id/image/:style/:basename.:extension',
                     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
   include ImageAsset
