51e54343e8d8e88cc0dfb5ab1419f7548a581f63
Paul Boiseau <paulboiseau@gmail.com>
2015-12-05 17:10:42 +0100

    test user controller refactor

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe UsersController, :type => :controller do
   login_user
   render_views
 
-  it 'show user profile page' do
-    user = create(:user)
-    get :show, id: user.id.to_s
+  describe 'GET show' do
+    it 'show user profile page' do
+      user = create(:user)
+      get :show, id: user.id.to_s
 
-    expect(response).to have_http_status(:success)
-    expect(response.body).to include('<span class="user-username">@' + user.username + '</span>')
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('<span class="user-username">@' + user.username + '</span>')
+    end
   end
 
 end
