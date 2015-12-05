e41d43950de991f6b8629d116e7e6e57a7634403
Paul Boiseau <paulboiseau@gmail.com>
2015-12-05 17:05:51 +0100

    test show user profile page

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
     username { Faker::Internet.user_name }
     email { "#{Faker::Internet.user_name}@example.com" }
     password Faker::Internet.password(10)
-    firstname Faker::Name.first_name
-    lastname Faker::Name.last_name
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
   end
 
   factory :post do
@@ -13,6 +13,9 @@ FactoryGirl.define do
     user
     factory :post_with_hashtags do
       message 'tweet with some hashtags #love #ruby'
+    end
+    factory :post_with_random_hashtags do
+      message { "#{Faker::Lorem.sentence(3)} ##{Faker::Lorem.word} ##{Faker::Lorem.word}" }
     end
   end
 
