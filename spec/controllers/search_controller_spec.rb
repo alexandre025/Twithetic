ce267180e12480cc01c5905814d8c0400e4be150
Paul Boiseau <paulboiseau@gmail.com>
2015-12-05 16:46:31 +0100

    test search specific tweet

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe SearchController, :type => :controller do
 
+  login_user
+  render_views
+
   it 'search content without typing anything' do
-    posts = create_list(:post, 10)
+    posts = create_list(:post, 5)
     post :search_results
 
     expect(response).to have_http_status(:success)
-    #TODO: expect to display all posts which have been created above
+    posts.each do |post|
+      expect(response.body).to include("post_#{post.id}")
+    end
+  end
+
+  it 'search a specific content' do
+    posts = create_list(:post, 5)
+    post :search_results, q: posts.first.message
+
+    expect(response).to have_http_status(:success)
+    expect(response.body).to include("post_#{posts.first.id}")
   end
 
 end
