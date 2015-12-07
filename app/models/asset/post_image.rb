67b850f22f0e1d6f4bf6eff011e86b9bb6528228
alexandre025 <alexandre.ferraille@gmail.com>
2015-12-07 21:14:01 +0100

    Post view WIP

--- app/models/asset/post_image.rb
+++ app/models/asset/post_image.rb
@@ -1,8 +1,8 @@
 class Asset::PostImage < Asset
 
   has_attached_file :attachment,
-                    styles: { thumb: '48x48>' },
-                    default_style: :thumb,
+                    styles: { thumb: '48x48>', regular: '800x800>' },
+                    default_style: :regular,
                     default_url: '/default/posts/:style/noimage.png',
                     url: '/twithetic/posts/:id/:style/:basename.:extension',
                     path: ':rails_root/public/twithetic/posts/:id/:style/:basename.:extension',
