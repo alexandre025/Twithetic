d7a1156082caf095c188622d727b10bc4138caae
alexandre025 <alexandre.ferraille@gmail.com>
2015-12-10 11:38:55 +0100

    Fix db

--- db/migrate/20151204203630_add_names_and_bio_to_users.rb
+++ db/migrate/20151204203630_add_names_and_bio_to_users.rb
@@ -2,6 +2,6 @@ class AddNamesAndBioToUsers < ActiveRecord::Migration
   def change
     add_column :users, :firstname, :string
     add_column :users, :lastname, :string
-    add_column :users, :bio, :string
+    add_column :users, :bio, :text
   end
 end
