9f4a103961c41dab51b1c8effdb7871bfaa564a5
alexandre025 <alexandre.ferraille@gmail.com>
2015-12-10 19:38:10 +0100

    Comments WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,27 @@ class CommentsController < ApplicationController
 
   before_action :authenticate_user!
 
-  before_action :load_resource, only: [:delete]
+  before_action :load_resource, only: [:destroy]
 
   def create
     @object = Comment.new(permitted_attributes)
     @object.post_id = params[:post_id]
     if @object.save
-      flash[:notice] = 'add_comment_success'
+      flash[:notice] = t('notif.notice.add_comment_success')
     else
-      flash[:error] = 'add_comment_error'
+      flash[:error] = t('notif.error.add_comment_error')
     end
     redirect_to post_path(@object.post)
   end
 
   def destroy
-    @object.destroy
-    flash[:notice] = 'delete_comment_success'
-    redirect_to post_path(@object.post)
+    post = @object.post
+    if can? :destroy, @object
+      @object.destroy!
+      redirect_to post_path(post), notice: t('notif.notice.delete_comment_success')
+    else
+      redirect_to post_path(post), error: t('notif.error.delete_comment_unauthorized')
+    end
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,14 @@
 class Comment < ActiveRecord::Base
 
+  # Scope
+  default_scope { order(created_at: :desc) }
+
   # Associations
   belongs_to :user
 
   belongs_to :post
+
+  #Validates
+  validates :message, length: { maximum: 140 }
 
 end
