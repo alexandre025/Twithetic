ce1e79d3a925cf2e056df58808df9bafba1c78e5
alexandre025 <alexandre.ferraille@gmail.com>
2015-12-11 15:34:36 +0100

    i18n implem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def current_locale
+    I18n.locale
+  end
+
+  def to_date(date,locale)
+    if locale == :fr
+      date.strftime('%d/%m/%Y')
+    else
+      date.strftime('%m/%d/%Y')
+    end
+  end
+
 end
